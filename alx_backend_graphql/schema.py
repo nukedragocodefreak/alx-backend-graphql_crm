377979b0b18bb190a04d38a8ef4ba931d5109f54
Noel Mangwarira <ncmangwarira@gmail.com>
2025-09-30 16:46:15 +0200

    fix bugs

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,7 @@
 import graphene
+from crm.schema import Query as CRMQuery
 
-class Query (graphene.ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
